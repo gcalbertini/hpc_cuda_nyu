e4c708b7a4340ddc4b08858520aec925f8823037
Guilherme A <gcalbertini@gmail.com>
2022-04-22 10:42:59 -0400

    Updates to reflect more elucidated thinking -- eigen vector included for consideration

--- hogwild.cu
+++ hogwild.cu
@@ -1,9 +1,14 @@
+#include <vector>
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
 #include <math.h>
 #include "utils.h"
-
 
 // Assume equation of form y= a + bx + cx^2 + dx^3 + ....
 // Squared L2 norm
@@ -40,11 +45,114 @@ void calc_gradient(double *train_x, double *train_y, double *weights, double *co
     }
 }
 
-void calc_pred(double *train_x)
+// decide if all double or all type long throughout?
+double *train_x(const unsigned long batch_size, const unsigned long numpredictors)
 {
+    /* Generate a new random seed from system time - do this once in your constructor */
+    srand(time(0));
 
-    // calculate the actual prediction using the gradients.
-    // Calculate it in mini-batch gradients and then update
+    double *train_x = (double *)aligned_malloc(batch_size * numpredictors * sizeof(double));
+    for (long i = 0; i < batch_size * numpredictors; i++)
+        train_x[i] = drand48();
+
+    return train_x;
 }
 
-// May want to create namespaces such such that we can use things like GPU::calc_gradient and CPU::calc_pred calc_gradient
+double *train_y(const unsigned long batch_size, const unsigned long numpredictors, double *train_x)
+{
+
+    double *train_y = (double *)(aligned_malloc(sizeof *train_y * batch_size));
+    // Define random generator with Gaussian distribution
+    double b = 123.;
+    const double mean = 0.0;
+    const double stddev = 0.2;
+    std::default_random_engine generator;
+    std::normal_distribution<double> dist(mean, stddev);
+
+    // Add Gaussian noise too
+    for (long i = 0; i < batch_size; i++)
+    {
+        for (long j = 0; j < numpredictors; j++)
+        {
+            train_y[j] = train_x[i * numpredictors + j] + b + dist(generator);
+        }
+    }
+
+    return train_y;
+}
+
+std::vector<std::vector<double>> train_x_csv()
+{
+    std::ifstream f;
+    std::vector<std::vector<double>> array; /* vector of vector<double>  */
+    std::string line, val;                  /* string for line & value */
+    long nrows = 0;
+    long ncols = 0;
+
+    f.open("generated_data/df_X.csv"); /* open file with filename as argument */
+    if (!f.is_open())
+    { /* validate file open for reading */
+        std::cerr << "error: file open failed!\n";
+        exit; // no effect!! change
+    }
+
+    while (std::getline(f, line))
+    {                                    /* read each line */
+        std::vector<double> v;           /* row vector v */
+        std::stringstream s(line);       /* stringstream line */
+        while (getline(s, val, ','))     /* get each value (',' delimited) */
+            v.push_back(std::stod(val)); /* add to row vector */
+        nrows++;
+        array.push_back(v); /* add row vector to array */
+        ncols++;
+    }
+
+    return array;
+}
+
+std::vector<double> train_y_csv()
+{
+    std::ifstream f;
+    std::string line, val; /* string for line & value */
+    std::vector<double> v;  
+    long nrows = 0;
+
+    f.open("generated_data/df_Y.csv"); /* open file with filename as argument */
+    if (!f.is_open())
+    { /* validate file open for reading */
+        std::cerr << "error: file open failed!\n";
+        exit; // no effect!! change
+    }
+
+    while (std::getline(f, line))
+    {                                    /* read each line */
+        std::stringstream s(line);       /* stringstream line */
+        while (getline(s, val, ','))     /* get each value (',' delimited) */
+            v.push_back(std::stod(val)); /* add to row vector */
+        nrows++;
+    }
+
+    return v;
+}
+
+int main()
+{
+    auto X = train_x_csv();
+    std::vector<std::vector<double>>::iterator it;
+    for (it = X.begin(); it != X.end(); it++)
+    {
+        // cout << (*it) << endl;
+        for (const auto &nexts : *it)
+            std::cout << nexts << std::endl;
+    }
+    // x checks out but needs to be converted to C array
+     std::cout << std::endl;
+    auto y = train_y_csv();
+    for (auto item : y)
+    {
+        std::cout << item << std::endl;
+    }
+    // y checks out but needs to be converted to C array
+
+    return 0;
+}
--- makefile
+++
@@ -1,9 +0,0 @@
-# https://stackoverflow.com/questions/9421108/how-can-i-compile-cuda-code-then-link-it-to-a-c-project
-all: program
-program: hogwild.o
-	g++ -o program -I/Users/gbert/anaconda3/include/ -I. -L/usr/local/cuda/lib64 -lcuda -lcudart -L/Users/gbert/anaconda3/Lib results.cpp hogwild.o 
-hogwild.o:
-	nvcc -c -arch=sm_62 hogwild.cu 
-clean: rm -rf *.o program
-
-# If you can somehow link in Python.h you win; try python3-config --ldflags or --cflags, locate Python.h
