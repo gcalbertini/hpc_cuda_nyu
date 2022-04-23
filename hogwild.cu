6a2c17236b9bfd3df76f56a195f70229cc3d36a8
mayukhghosh <mayukhghosh9@gmail.com>
2022-04-23 14:09:38 -0400

    Refactored code

--- hogwild.cu
+++ hogwild.cu
@@ -37,44 +37,36 @@ template <typename T> void shuffleXY(T x, T y, size_t n, size_t k) {
 // Loss for single example = (y* - y)^2
 // Derivative of loss wrt coeff_i = 2(y* - y)*(i*coeffs[i]*predictor_values[i]^(i-1))
 
-float difflinear(long numpredictors, double* coeffs, double* predictor_values, long desired_coeff, double pred, double train_y){
+double difflinear(double* weights, double predictor_value, long desired_coeff, double pred, double train_y){
     if (desired_coeff == 0)
         return 2*(pred - train_y);
 
-    return 2*(pred - train_y)*(desired_coeff*coeffs[desired_coeff]*pow(predictor_values[desired_coeff], desired_coeff-1));
+    return 2*(pred - train_y)*(desired_coeff*weights[desired_coeff]*pow(predictor_value, desired_coeff-1));
 
 }
 
+// calculate gradient for each batch
+void calc_gradient(double* train_x, double* train_y, int batch_size, long numpredictors, double* weights, double* pred, double* w_gradients, double* b_gradient){
 
-void calc_gradient(double* train_x, double* train_y, double* weights, double* constant, long batch_size, long numpredictors, double* pred){
-
-    double* temp_x = malloc((numpredictors)*size_of(double));
-    double* w_gradients = malloc(numpredictors*size_of(double));
-    double b_gradient = 0;
-
-    for(long i = 0; i<batch_size; i++){
-        for(long j = 0; j<numpredictors; j++){
-            temp_x[j] = train_x[i*numpredictors+j];
-        }
-
-        for(long k = 1 ; k<=numpredictors; k++){
-            w_gradients[k] += difflinear(numpredictors, weights, temp_x, k, pred[i], train_y[i]);
-        }
-        b_gradient += difflinear(numpredictors, weights, temp_x, 0, pred[i], train_y[i]);
+    for (int b=0;b<batch_size;b++){
+    for(long k = 1 ; k<=numpredictors; k++){
+        w_gradients[k] += difflinear( weights, train_x[b*numpredictors+k], k, pred[b], train_y[b]);
+    }
+    b_gradient[0] += difflinear(weights, train_x[0], 0, pred[b], train_y[b]);
     }
 
 
 }
 
-void calc_pred(double* train_x, double* weights, long batch_size, double* w_gradients, double b_gradient, long numpredictors, double learning_rate, double* pred){
+void calc_pred(double* train_x, double* weights, int batch_size, double* w_gradients, double* b_gradient, long numpredictors, double learning_rate, double* pred){
 
     //calculate the actual prediction using the gradients.
     //Calculate it in mini-batch gradients and then update
 
     // update weights based using the gradients
-    weights[0] = weights[0] - b_gradient * learning_rate;
+    weights[0] = weights[0] - (b_gradient[0]/batch_size) * learning_rate;
     for (long i = 0; i<numpredictors; i++) {
-        weights[i+1] = weights[i+1] - w_gradients[i] * learning_rate;
+        weights[i+1] = weights[i+1] - (w_gradients[i]/batch_size) * learning_rate;
     }
 
     // update prediction using the new weights
@@ -87,4 +79,71 @@ void calc_pred(double* train_x, double* weights, long batch_size, double* w_grad
         }
         pred[i] = pred_reduction_sum;
     }
+}
+
+int main(int argc, char *argv[])
+{
+    long numpredictors;
+    int batch_size;
+    long train_size;
+    int num_epochs;
+    double learning_rate = 0.05;
+
+    train_size = atol(argv[1]);
+    numpredictors = atol(argv[2]);
+    batch_size = atoi(argv[3]);
+    num_epochs = atoi(argv[4]);
+
+
+    
+
+    auto X = train_x_csv();
+    std::vector<std::vector<double>>::iterator it;
+    for (it = X.begin(); it != X.end(); it++)
+    {
+        // cout << (*it) << endl;
+        for (const auto &nexts : *it)
+            std::cout << nexts << std::endl;
+    }
+    // x checks out but needs to be converted to C array
+     std::cout << std::endl;
+    auto y = train_y_csv();
+    for (auto item : y)
+    {
+        std::cout << item << std::endl;
+    }
+    // y checks out but needs to be converted to C array
+
+    ///Assume the above is implemented
+
+    /// todo: initialize random weights and gradients
+    double *w_gradients = (double *) malloc(sizeof(double) * numpredictors);
+    double *b_gradient = (double *) malloc(sizeof(double));
+    double *weights = (double *) malloc(sizeof(double) * (numpredictors+1));
+    ///
+
+
+
+    double *train_batch_x = (double *) malloc(sizeof(double) * batch_size * numpredictors);
+    double *train_batch_y = (double *) malloc(sizeof(double) * batch_size);
+    double *pred = malloc(sizeof(double) * batch_size);
+    long start =0;
+
+    for(int epoch=0; epoch<num_epochs;epoch++){
+        for(long i=0; i<train_size; i++){
+            for(long j=0; j<numpredictors; j++){
+                train_batch_x[start*numpredictors + j] = X[i*numpredictors + j];
+            }
+            train_batch_y[start] = y[i];
+            start++;
+            if ((i+1)%batch_size == 0){
+                calc_pred(train_batch_x, weights, batch_size, w_gradients, b_gradient, numpredictors, learning_rate, pred);
+                calc_gradient(train_batch_x, train_batch_y, batch_size, numpredictors, weights, pred, w_gradients, b_gradient);
+                start = 0;
+            }
+
+        }
+    }
+
+    return 0;
 }
